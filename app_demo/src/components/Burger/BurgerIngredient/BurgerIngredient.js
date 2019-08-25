6ebe2fc49930122a185355488304326012d08604
thloc <mail.tranhuuloc@gmail.com>
2019-08-25 19:52:36 +0700

    Setup layout

---
+++ app_demo/src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import ProTypes from 'prop-types';
+
+import './BurgerIngredient.css';
+
+class BurgerIngredient extends Component {
+    render() {
+        let ingredient = null;
+
+        switch (this.props.type) {
+            case ('bread-bottom'):
+                ingredient = <div className="BreadBottom"></div>;
+                break;
+            case ('bread-top'):
+                ingredient = (
+                    <div className="BreadTop">
+                        <div className="Seeds1"></div>
+                        <div className="Seeds2"></div>
+                    </div>
+                );
+                break;
+            case ('meat'):
+                ingredient = <div className="Meat"></div>;
+                break;
+            case ('cheese'):
+                ingredient = <div className="Cheese"></div>;
+                break;
+            case ('bacon'):
+                ingredient = <div className="Bacon"></div>;
+                break;
+            case ('Salad'):
+                ingredient = <div className="Salad"></div>;
+                break;
+            default:
+                ingredient = null;
+        }
+
+        return ingredient;
+    }
+
+};
+
+// BurgerIngredient.prototype = {
+//     type: ProTypes.string.isRequired
+// }
+
+export default BurgerIngredient;
---
+++ app_demo/src/components/Layout/Layout.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Aux from '../../hoc/_Aux';
+import './Layout.css';
+
+const layout = (props) => (
+    <Aux>
+        <div>Toolbar, SideDrawer, Backdrop</div>
+        <main className="Content">
+            { props.children }
+        </main>
+    </Aux>
+);
+
+export default layout;
